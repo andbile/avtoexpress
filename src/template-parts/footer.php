b51d18b5f4af5add627aab65ee11c408c124c5ef
Andrey Biletsky <andbile@gmail.com>
2019-12-23 22:41:48 +0200

    Added main-footer

--- src/template-parts/footer.php
+++ src/template-parts/footer.php
@@ -1,1 +1,87 @@
+<footer class="main-footer">
+
+    <div class="main-footer__top-link-wrp">
+        <div class="link-item link-line link-line--grey">
+            <div class="link-item__links_wrp">
+                <span class="icon icon-steering_wheel"></span>
+                <a href="">Тест-драйв</a>
+            </div>
+        </div>
+
+
+        <div class="link-item">
+            <div class="link-item__links_wrp link-line link-line--grey">
+                <span class="icon icon-speedometer"></span>
+                <a href="">Трейд-ин оценка</a>
+            </div>
+        </div>
+
+
+
+        <div class="link-item">
+            <div class="link-item__links_wrp link-line link-line--grey">
+                <span class="icon icon-rocket"></span>
+                <a href="">Конфигуратор</a>
+            </div>
+        </div>
+
+
+        <div class="link-item">
+            <div class="link-item__links_wrp link-line link-line--grey">
+                <span class="icon icon-car"></span>
+                <a href="">Модельный ряд</a>
+            </div>
+        </div>
+
+        <div class="link-item">
+            <div class="link-item__links_wrp link-line link-line--grey">
+                <span class="icon icon-palm"></span>
+                <a href="">Запись на сервис</a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <div class="main-footer__bot container">
+
+        <div class="col-left">
+
+            <div class="copyright__wrp">
+                <div class="logo">Автоэкспресс</div>
+                <div class="text">Официальный дилер</div>
+                <div class="copyright">
+                    © 2011-2017 <br>Все права защищены.
+                </div>
+            </div>
+
+
+            <nav class="main-footer__nav">
+                <ul>
+                    <li><a href="">О компании</a></li>
+                    <li><a href="">Об авто</a></li>
+                    <li><a href="">Услуги</a></li>
+                    <li><a href="">Автозаметки</a></li>
+                    <li><a href="">Сервис</a></li>
+                    <li><a href="">Акции</a></li>
+                    <li><a href="">Галерея</a></li>
+                    <li><a href="">Контакты</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+        <div class="main-footer__contact">
+            <div class="tel"><a href="tel:+74932334444">+7 (4932) 33-44-44</a></div>
+            <p class="address">г. Иваново, ул. Калашникова, д. 26а <br>(р-н Минеевского моста, справа от завода ИвТекМаш)</p>
+            <div class="social">
+                <a href="">Одноклассники</a>
+                <a href="">Мой мир</a>
+                <a href="">Вконтакте</a>
+            </div>
+        </div>
+
+    </div>
+</footer>
 
---
+++ src/template-parts/privacy_policy.php
@@ -0,0 +1,9 @@
+<footer class="main-footer__privacy-policy container">
+    <div class="col-left">
+        <a>Политика конфиденциальности</a>
+        <a>Пользовательское соглашение</a>
+    </div>
+    <div class="col-right">
+        <a href="">Разработано Йод</a>
+    </div>
+</footer>
