62c5ae9d1681e3f6cf8dbf6ac9842e8e67d548f3
Andrey Biletsky <andbile@gmail.com>
2019-12-26 21:34:58 +0200

    Added section links in model_page.php

--- src/model_page.php
+++ src/model_page.php
@@ -1,1 +1,56 @@
-<?php
+<?php require_once 'template-parts/page_head.php' ?>
+<!-- top main menu -->
+<? require_once 'template-parts/header.php' ?>
+<main>
+<section class="mp-main_preview container">
+        <div class="mp-main_preview__img"><img src="img/model_big_01.png" alt="Фото машины"></div>
+
+        <div class="mp-main_preview__content">
+
+            <div class="mp-main_preview__link link-line">
+                <span class="icon icon-path_left"></span>
+                <a href="brand_page.php">LADA Granta</a>
+            </div>
+
+            <div class="mp-main_preview__model">Лифтбек Норма Платина</div>
+            <div class="mp-main_preview__price">от 499 400 &#x20bd;</div>
+            <div class="btn btn--orange"><a href="">Записаться на тест-драйв</a></div>
+        </div>
+
+
+    </section>
+
+<section class="mp-links container">
+    <div class="mp-links__col-1">
+
+        <div class="mp-link link-line link-line--grey">
+            <span class="icon icon-steering_wheel"></span>
+            <a href="">Заявка на кредит</a>
+        </div>
+
+        <div class="mp-link link-line link-line--grey">
+            <span class="icon icon-speedometer"></span>
+            <a href="">Заявка на покупку</a>
+        </div>
+
+        <div class="mp-link link-line link-line--grey">
+            <span class="icon icon-rocket"></span>
+            <a href="">Аксессуары</a>
+        </div>
+
+    </div>
+
+    <div class="mp-links__col-2">
+        <div class="mp-link--type2"><a href="">Характеристики</a></div>
+        <div class="mp-link--type2"><a href="">Комплектация</a></div>
+    </div>
+</section>
+
+
+
+</main>
+<!-- footer -->
+<?php require_once 'template-parts/footer.php'?>
+<?php require_once 'template-parts/privacy_policy.php' ?>
+<?php require_once 'template-parts/page_bottom.php' ?>
+
