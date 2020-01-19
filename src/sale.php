6ca81dca9734aa76c795c40d495d46fb3de6aa83
Andrey Biletsky <andbile@gmail.com>
2020-01-19 22:58:37 +0200

    Added adaptive sale.php

--- src/sale.php
+++ src/sale.php
@@ -21,7 +21,7 @@
         <div class="bars__row bar--m-bottom">
 
             <div class="bar__common bar__two-thirds bar--m-right">
-                <div class="bar__img"><img src="img/bar_2-3_01.jpg" alt="Фото автомобиля"></div>
+                <div class="bock_img-full" data-bock-img--full><img src="img/bar_2-3_01.jpg" alt="Фото автомобиля"></div>
                 <div class="bar__content-wrp">
                     <div class="bar__content">
                         <h2 class="content__title">Газель Некст<br> от 333 ₽ в день</h2>
@@ -33,7 +33,7 @@
 
 
             <div class="bar__common bar_third">
-                <div class="bar__img"><img src="img/bar_1-3_01.jpg" alt="Фото автомобиля"></div>
+                <div class="bock_img-full" data-bock-img--full><img src="img/bar_1-3_01.jpg" alt="Фото автомобиля"></div>
                 <div class="bar__content-wrp">
                     <div class="bar__content">
                         <h2 class="content__title">Городской формат для бизнеса</h2>
@@ -48,7 +48,7 @@
         <div class="bars__row bar--m-bottom">
 
             <div class="bar__common bar__full">
-                <div class="bar__img"><img src="img/bar_full_01.jpg" alt="Фото автомобиля"></div>
+                <div class="bock_img-full"  data-bock-img--full><img src="img/bar_full_01.jpg" alt="Фото автомобиля"></div>
                 <div class="bar__content-wrp">
                     <div class="bar__content">
                         <h2 class="content__title">Бонус от нашего партнера</h2>
@@ -65,7 +65,7 @@
 
 
             <div class="bar__common bar_third bar--m-right">
-                <div class="bar__img"><img src="img/bar_1-3_02.jpg" alt="Фото автомобиля"></div>
+                <div class="bock_img-full" data-bock-img--full><img src="img/bar_1-3_02.jpg" alt="Фото автомобиля"></div>
                 <div class="bar__content-wrp">
                     <div class="bar__content">
                         <h2 class="content__title">Мы дарим вам бонусы</h2>
@@ -77,7 +77,7 @@
 
 
             <div class="bar__common bar__two-thirds">
-                <div class="bar__img"><img src="img/bar_2-3_02.jpg" alt="Фото автомобиля"></div>
+                <div class="bock_img-full" data-bock-img--full><img src="img/bar_2-3_02.jpg" alt="Фото автомобиля"></div>
                 <div class="bar__content-wrp">
                     <div class="bar__content">
                         <h2 class="content__title">Бонус от нашего<br> партнера</h2>
