bc70fea3d4bf6ef64e06e3174e21ccd19ffb7a22
andreybile <end38n7z>
2020-01-31 11:22:07 +0200

    Finished layout current_model_used.php

--- src/current_model_used.php
+++ src/current_model_used.php
@@ -5,7 +5,6 @@
 <section class="section-top-current-model-used container">
 
         <div class="col-1">
-
             <div class="section-top-cmu__wrp">
                 <div class="link-wrp link-line">
                     <span class="icon icon-path_left"></span>
@@ -24,16 +23,13 @@
 
         <div class="model-used-slider">
 
-
-            <div class="slider__wrapper" data-slider-car-used> <!---->
-
+            <div class="slider__wrapper" data-slider-car-used>
 
                 <div class="slider-full-img slider-full-img--model-used bock_img-full" data-bock-img--full>
                     <img src="img/car_used_slider_01.jpg" alt="Превью фото машины">
                 </div>
 
-                
-                
+
                 <div class="slider-btn-wrp slider-btn-wrp---model-used">
                     <span class="slider-btn slider-btn--prev slider-btn--model-used" data-prev></span>
                     <span class="slider-btn slider-btn--next slider-btn--model-used" data-next></span>
@@ -83,23 +79,267 @@
                         </div>
                     </div>
 
-
-
                 </div>
             </div>
-            
-            
-            
+        </div>
+    </section>
 
+
+<section class="mp-specifications mp-specifications--current-model-used container">
+
+        <div class="mp-specifications__col-1" id="t_spec">
+            <div class="content__title">Характеристики</div>
+
+            <table class="mp-specifications__table">
+                <tr>
+                    <td>Марка</td>
+                    <td class="table__content">Cherry</td>
+                </tr>
+                <tr>
+                    <td>Модель</td>
+                    <td class="table__content">Tiggo</td>
+                </tr>
+                <tr>
+                    <td>Комплектация</td>
+                    <td class="table__content">—</td>
+                </tr>
+                <tr>
+                    <td>Цвет</td>
+                    <td class="table__content">Серебристый</td>
+                </tr>
+                <tr>
+                    <td> Год выпуска</td>
+                    <td class="table__content">2009</td>
+                </tr>
+                <tr>
+                    <td>Кузов</td>
+                    <td class="table__content">Внедорожник 5 дв.</td>
+                </tr>
+                <tr>
+                    <td> Тип двигателя</td>
+                    <td class="table__content">Бензин</td>
+                </tr>
+                <tr>
+                    <td>Привод</td>
+                    <td class="table__content">—</td>
+                </tr>
+                <tr>
+                    <td>Объем двиг.</td>
+                    <td class="table__content">—</td>
+                </tr>
+                <tr>
+                    <td>Коробка</td>
+                    <td class="table__content">—</td>
+                </tr>
+                <tr>
+                    <td>Мощность двиг.</td>
+                    <td class="table__content">—</td>
+                </tr>
+
+                <tr>
+                    <td>Цена</td>
+                    <td class="table__content">290 000 &#x20bd;</td>
+                </tr>
+            </table>
+        </div>
+
+
+        <div class="mp-specifications__col-2">
+            <div class="content__title" id="t_equipment">Комплектация</div>
+
+            <div class="mp-specifications__list-wrp">
+
+                <ul class="list-with-squares">
+                    <li>Музыка</li>
+                    <li>Сигнализация(GPS)</li>
+                    <li>Центральный замок</li>
+                    <li>Электропакет</li>
+                    <li>Климат-кондиционер</li>
+                    <li>Бортовой компьютер</li>
+                    <li>Подогрев зеркал</li>
+                </ul>
+
+                <ul class="list-with-squares">
+                    <li>ГУР</li>
+                    <li>ABS</li>
+                    <li>2SRS</li>
+                    <li>2 владельца</li>
+                    <li>ПТС-оригинал</li>
+                    <li>Отличное состояние</li>
+                </ul>
+            </div>
+
+            <div class="content__text">
+                <p>Оригинальный пробег. ПТС-оригинал. Проведена полная комплексная техническая диагностика- двигатель и коробка работает без нареканий.</p>
+                <p>Приобретая автомобиль в нашей компании Вы получаете ряд преимуществ:</p>
+                <p>- Гарантия юридической чистоты! Автомобиль не в залоге, не в кредите и не имеет других обременений;</p>
+                <p>- отсутствие скрытых платежей и дополнительных комиссий;</p>
+                <p> - предоставляется тест-драйв на автомобиле перед покупкой;</p>
+                <p>- индивидуальные условия для каждого клиента.</p>
+            </div>
+
+        </div>
+
+    </section>
+
+<section class="mp-links--2 container">
+        <div class="mp-links__col-1">
+
+            <div class="mp-link link-line link-line--grey" data-action="modal_window" data-target="request_credit">
+                <span class="icon icon-steering_wheel"></span>
+                <a href="">Заявка на кредит</a>
+            </div>
+
+            <div class="mp-link link-line link-line--grey" data-action="modal_window" data-target="make_request">
+                <span class="icon icon-speedometer"></span>
+                <a href="">Заявка на покупку</a>
+            </div>
 
         </div>
 
 
+    <div class="btn btn--orange"><a href="test_drive.php">Записаться на тест-драйв</a></div>
 
-        
+    </section>
+
+
+    <section class="mp-information-slider container">
+        <div class="information-title_wrp">
+            <h2 class="content__title">Новости и события</h2>
+            <div class="content__link">
+                <a href="">Смотреть все новости</a>
+                <span class="icon icon-path_right"></span>
+            </div>
+        </div>
+
+        <div class="slider__wrapper" data-slider-information>
+
+            <div class="slider-btn-wrp  slider-btn-wrp--information">
+                <span class="slider-btn slider-btn--prev" data-prev></span>
+                <span class="slider-btn slider-btn--next" data-next></span>
+            </div>
+
+            <div class="slider-items-wrp">
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_01.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">ГАЗель Некст от 333 руб. в день</p>
+                            <p  class="information_description">Данная программа призвана обезопасить клиента, который …</p>
+                            <p class="information_description__date">27 сентября 2016</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_02.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">Лифан в Иваново, цены и комплектации</p>
+                            <p  class="information_description">С появлением на отечественном авторынке такой марки как …</p>
+                            <p class="information_description__date">3 марта 2017</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_03.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">Конкурс «Мисс ЛАДА» Иваново </p>
+                            <p  class="information_description">21 августа состоялся яркий полуфинал конкурса «Автоледи …</p>
+                            <p class="information_description__date">21 августа 2016</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_04.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">Пятидесятилетие «АвтоВаза». Как это было!</p>
+                            <p  class="information_description">В субботу, 16 июля, по всей России официальные дилеры LADA …</p>
+                            <p class="information_description__date">16 июля 2016</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_01.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">ГАЗель Некст от 333 руб. в день</p>
+                            <p  class="information_description">Данная программа призвана обезопасить клиента, который …</p>
+                            <p class="information_description__date">27 сентября 2016</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="slide-item slide-item--information">
+                    <a href="" class="information__item">
+                        <div class="information__img">
+                            <img  src="img/information_preview_02.jpg" alt="Полезная информация">
+                        </div>
+                        <div class="information__content">
+                            <p class="information__title">ГАЗель Некст от 333 руб. в день</p>
+                            <p  class="information_description">Данная программа призвана обезопасить клиента, который …</p>
+                            <p class="information_description__date">27 сентября 2016</p>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+
+
 
 
     </section>
+
+    <section class="section-map">
+        <script src="https://api-maps.yandex.ru/2.1/?apikey=40b8a09d-8389-4a22-a0ac-2e18995eae34&lang=ru_RU"
+                type="text/javascript">
+        </script>
+        <div id="map">
+
+        </div>
+
+        <script>
+            ymaps.ready(function () {
+                var myMap = new ymaps.Map("map", {
+                    center: [57.026488, 40.968522],
+                    zoom: 16,
+                    controls: []
+                });
+
+
+                var myPlacemark = new ymaps.GeoObject({
+                    geometry: {
+                        type: "Point",
+                        coordinates:  [57.026488, 40.968522]
+                    },
+                    properties: {
+                        // Контент метки.
+                        hintContent: 'г. Иваново, ул. Калашникова, д. 26а'
+                    }
+                });
+
+                myMap.geoObjects.add(myPlacemark);
+            });
+        </script>
+    </section>
+
 
 
 </main>
